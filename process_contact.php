2e96add361a33137b3092ef501a821c8ba5fbe87
mauricio <mauricioafcoproscki@gmail.com>
2025-06-01 17:17:48 -0300

    teste

--- process_contact.php
+++ process_contact.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
 error_reporting(0);
 
+require 'vendor/autoload.php';
+
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
